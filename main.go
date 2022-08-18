bf366851d0f7a4f9b54ea45c2658299cb69453b3
aaleike <aaleike@tencent.com>
2022-08-18 17:08:05 +0800

    2

--- main.go
+++ main.go
@@ -2,12 +2,10 @@ package main
 
 import (
 	"log"
-	"main/room"
-	socket "main/websocket"
 )
 
 func main() {
 	log.SetFlags(log.Ldate | log.Ltime)
-	e := socket.Engine{Rms: make(map[string]*room.Room)}
+	e := Engine{Rms: make(map[string]*Room)}
 	e.Initserver("3000")
 }
